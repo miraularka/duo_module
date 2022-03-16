f7b69e572c25e07e935455d7e595bc6e2a18923f
Mirau <tehl33tjim@gmail.com>
2022-03-15 22:19:46 -0400

    Initial Upload

---
+++ duo.py
@@ -0,0 +1,18 @@
+#.-----------------------------.
+#| Duolingo Module for Polybar |
+#'-----------------------------'
+import duolingo
+usr = ""
+pas = ""
+lingo  = duolingo.Duolingo(usr, pas)
+
+info = lingo.get_streak_info()
+streak = info['streak_extended_today']
+if streak == False:
+    full = '%{F#FF0000}%{F-}'
+if streak == True:
+    full = '%{F#00FF00}%{F-}'
+days = str(info['site_streak'])
+full = "Duolingo ("+full+")-("+days+")%{F-}"
+
+print(full)
